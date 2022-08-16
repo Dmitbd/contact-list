91111800b5e5016c9129d0209fc5f87b739fd749
Dmitbd <mymaughem@gmail.com>
2022-08-16 14:12:03 +0300

    добавить FormInput в popupWithEditUser

--- src/components/PopupWithEditUser.tsx
+++ src/components/PopupWithEditUser.tsx
@@ -1,13 +1,16 @@
 import { useEffect } from "react"
-import { useState } from "react"
 import { editUserAsync } from "../api/axios"
 import { useAppDispatch, useAppSelector } from "../hooks/hooks"
 import { editUserPopupIsOpen } from "../store/openPopupSlice"
 import PopupWithForm from "./PopupWithForm"
 import { isAlert } from "../store/popupWithAlert"
-import { EditUserForm } from "../types/types"
+import { useForm } from "react-hook-form"
+import FormInput from "./FormInput"
+import { requaredField } from "../utils/constants/formTextConstants"
 
 const PopupWithEditUser: React.FC = () => {
+
+  const { handleSubmit, control, setValue, getValues } = useForm({ mode: 'onChange' })
 
   const dispatch = useAppDispatch()
   const popupOpener = useAppSelector(state => state.popupOpener.editUserPopup)
@@ -17,26 +20,16 @@ const PopupWithEditUser: React.FC = () => {
     dispatch(editUserPopupIsOpen(false))
   }
 
-  const [userEditForm, setUserEditForm] = useState<EditUserForm>({
-    id: 0,
-    username: '',
-    email: ''
-  })
-
   useEffect(() => {
-    setUserEditForm({
-      ...userEditForm,
-      id: userAuthData.user.id,
-      username: userAuthData.user.username,
-      email: userAuthData.user.email
-    })
+    setValue('username', userAuthData.user.username)
+    setValue('email', userAuthData.user.email)
   }, [popupOpener])
 
-
-  const editUser: React.FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault()
-    if (userEditForm.username !== userAuthData.user.username || userEditForm.email !== userAuthData.user.email) {
-      editUserAsync(userEditForm, dispatch)
+  const editUser = (userData: any) => {
+    const inputValues = getValues(['username', 'email'])
+    if (inputValues[0] !== userAuthData.user.username || inputValues[1] !== userAuthData.user.email) {
+      userData.id = userAuthData.user.id
+      editUserAsync(userData, dispatch)
       dispatch(isAlert({ isOpen: true, alertText: 'Данные изменились' }))
       dispatch(editUserPopupIsOpen(false))
     } else {
@@ -44,14 +37,27 @@ const PopupWithEditUser: React.FC = () => {
     }
   }
 
-  const handleChangeInput: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setUserEditForm({ ...userEditForm, [e.target.name]: e.target.value })
-  }
-
   return (
-    <PopupWithForm isOpen={popupOpener} onClose={handlerPopupClose} onSubmit={editUser} title={'Редактировать профиль'} buttonText={'Изменить'} name={'edit-user'}>
-      <input className="input-blue" type='text' placeholder='Username' value={userEditForm.username} name='username' onChange={handleChangeInput} ></input>
-      <input className="input-blue" type='text' placeholder='Email' value={userEditForm.email} name='email' onChange={handleChangeInput} ></input>
+    <PopupWithForm isOpen={popupOpener} onClose={handlerPopupClose} onSubmit={handleSubmit(editUser)} title={'Редактировать профиль'} buttonText={'Изменить'} name={'edit-user'}>
+
+      <FormInput
+        name="username"
+        placeholder="username"
+        control={control}
+        rules={{
+          required: requaredField,
+        }}
+      />
+
+      <FormInput
+        name="email"
+        placeholder="email"
+        control={control}
+        rules={{
+          required: requaredField
+        }}
+      />
+
     </PopupWithForm>
   )
 }
