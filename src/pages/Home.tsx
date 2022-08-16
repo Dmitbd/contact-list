9f5df546d9eae0568c76e0b059b48678634af0e6
Dmitbd <mymaughem@gmail.com>
2022-08-16 13:18:40 +0300

    добавить image на страницу home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom"
+import image from '../images/main-book.svg'
 
 const Home: React.FC = () => {
   return (
-    <div className="inputs-container">
-      <p className="text-white">Войти под существующим email</p>
+    <section className="m-auto pt-8 flex flex-col">
+      <h1 className="text-center text-white text-2xl cursor-default">Телефонная книга</h1>
+      <img className="m-auto" src={image} alt="телефонная книга"></img>
+      <p className="text-white text-white text-center cursor-default">Войти под существующим email</p>
       <Link className="btn-blue" to="/login">Войти</Link>
-      <p className="text-white">Зарегистрировать новый email</p>
+      <p className="text-white text-white text-center cursor-default">Зарегистрировать новый email</p>
       <Link className="btn-blue" to="/signup">Регистрация</Link>
-    </div>
+    </section>
   )
 }
 
